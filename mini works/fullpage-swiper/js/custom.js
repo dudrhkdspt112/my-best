0dab5106d5d1dca1e337b98ae7573436c7a6ba9a
dudrhkd165 <dudrhkdspt165@gmail.com>
2024-06-23 19:24:55 +0900

    Initial commit

--- mini works/fullpage-swiper/js/custom.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".fullpages", {
-    direction: "vertical",
-    slidesPerView: 1,
-    mousewheel: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
--- works/fred pizza/js/jquerystyle.js
+++
@@ -1,7 +0,0 @@
-$('.right_images').mouseenter(function(){
-    $('.mouseevent').slideDown(400)
-  })
-  $('.right_images').mouseleave(function(){
-    $('.mouseevent').slideUp(400)
-    })
-    
